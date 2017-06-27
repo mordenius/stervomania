dfdfb9b4930bdf5a3ca509ccdf84025176836dc6
Fa-got <alexandr.oliinyk@gmail.com>
2017-06-27 19:06:44 +0300

    styled menu

--- source/js/view/components/main.js
+++ source/js/view/components/main.js
@@ -4,15 +4,12 @@ class Main extends Component {
   render() {
     return (
       <div className="container">
-        <h1>MAIN</h1>
-        <ul>
-          <li onClick={()=>this.goLink("/One")}>One</li>
-          <li onClick={()=>this.goLink("/Two")}>Two</li>
-          <li onClick={()=>this.goLink("/Three")}>Three</li>
-          <li onClick={()=>this.goLink("/Four")}>Four</li>
-          <li onClick={()=>this.goLink("/Five")}>Five</li>
-          <li onClick={()=>this.goLink("/Six")}>Six</li>
-        </ul>
+          <div onClick={()=>this.goLink("/One")}></div>
+          <div onClick={()=>this.goLink("/Two")}></div>
+          <div onClick={()=>this.goLink("/Three")}></div>
+          <div onClick={()=>this.goLink("/Four")}></div>
+          <div onClick={()=>this.goLink("/Five")}></div>
+          <div onClick={()=>this.goLink("/Six")}></div>
       </div>
     );
   }
