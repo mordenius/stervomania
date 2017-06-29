048d3b67a4416c5f30aed4e4980c0285796988c4
mordenius <mordenius@gmail.com>
2017-06-29 20:04:21 +0300

    dev(webpack.config)

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,42 +1,41 @@
-'use strict';
-const webpack = require('webpack');
-const path = require('path');
-
-module.exports = { 
-  entry: [__dirname + '/source/js/index', __dirname + '/node_modules/webpack/hot/dev-server'],
-  output: {
-    path:  __dirname + '/build/',
-    publicPath: '/build/',
-    filename: 'main.min.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.sass$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'] 
-      },
-      {
-        test:   /\.(png|jpg|gif|svg|ttf|eot|woff|woff2|otf)$/,
-        exclude: /node_modules/,
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]'
-        }
-      },
-      {
-        test : /\.jsx?/,
-        exclude: /node_modules/,
-        use: ['react-hot-loader', 'babel-loader']
-      },
-      { 
-        test: /\.json$/, 
-        use: 'json-loader'
-      }
-    ],
-  },
-  devServer: {
-    contentBase: './',
-    inline: true,
-    hot: true
-  }
-}
+module.exports = {
+	entry: [
+		__dirname + "/source/js/index",
+		__dirname + "/node_modules/webpack/hot/dev-server"
+	],
+	output: {
+		path: __dirname + "/build/",
+		publicPath: "/build/",
+		filename: "main.min.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?/,
+				exclude: /node_modules/,
+				use: ["react-hot-loader", "babel-loader"]
+			},
+			{
+				test: /\.json$/,
+				use: "json-loader"
+			},
+			{
+				test: /\.sass$/,
+				use: ["style-loader", "css-loader", "sass-loader"]
+			},
+			{
+				test: /\.(png|jpg|gif|svg|ttf|eot|woff|woff2|otf)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "file-loader",
+					options: "[path][name].[ext]"
+				}
+			}
+		]
+	},
+	devServer: {
+		contentBase: "./",
+		inline: true,
+		hot: true
+	}
+};
